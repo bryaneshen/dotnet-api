f7bca45330436091fcfaa4fb247b6bd75280cf53
Bryan Shen <beshen@sfu.ca>
2023-03-24 16:33:59 -0700

    added comments under migration file to understand it more

--- Commander/Migrations/20230324225646_InitialMigration.cs
+++ Commander/Migrations/20230324225646_InitialMigration.cs
@@ -8,16 +8,20 @@ namespace Commander.Migrations
         // This method creates stuff
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            // creates a table called Commands
             migrationBuilder.CreateTable(
                 name: "Commands",
                 columns: table => new
-                {
+                { 
+                    // Adding columns to the table here.
+                    // the ID will be incremented by 1 automatically 
                     Id = table.Column<int>(nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     HowTo = table.Column<string>(nullable: true),
                     Line = table.Column<string>(nullable: true),
                     Platform = table.Column<string>(nullable: true)
                 },
+                // adding constraint to table, setting the id as the primary key
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Commands", x => x.Id);
