da9756f00718a2185c3cfa2cf068c6bb8c85c7ba
Bryan Shen <beshen@sfu.ca>
2023-04-02 22:36:34 -0700

    setup more PATCH request things

--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace Commander
 {
@@ -30,7 +31,9 @@ namespace Commander
             services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer
                 (Configuration.GetConnectionString("CommanderConnection")));
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(s => {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
 
             // adding our registration for ICommanderRepo and MockCommanderRepo into the services container.
             services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
